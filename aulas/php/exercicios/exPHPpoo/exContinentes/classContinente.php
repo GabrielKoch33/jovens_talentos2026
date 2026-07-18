63884bc74a6e705545d7efe725b5c00912e40cc4
Gabriel Koch da Silva <gabrielkochsilva@gmail.com>
2026-07-18 15:00:55 -0300

    finalizado exercicio continente

--- aulas/php/exercicios/exPHPpoo/exContinentes/classContinente.php
+++ aulas/php/exercicios/exPHPpoo/exContinentes/classContinente.php
@@ -11,14 +11,79 @@ class Continente {
 
     public function adicionaPais($oObjPais){
         $this->listaPaises[] = $oObjPais;
+        return $oObjPais->getNome(). ' adicionado!';
     }
 
     public function dimensaoTotal(){
         $fTotDimensao = 0;
-        foreach($listaPaises as $pais){
-            $this->pais->get
+        foreach($this->listaPaises as $pais){
+            $fTotDimensao += $pais->getDimensao();
         }
+        return $fTotDimensao;
     }
 
+    public function populacaoTotal(){
+        $fTotPopulacao = 0;
+        foreach($this->listaPaises as $pais){
+            $fTotPopulacao += $pais->getPopulacao();
+        }
+        return $fTotPopulacao; 
+    }
+    
+    public function densidadeTotal(){
+        return $fTotDensidade = $this->populacaoTotal() / $this->dimensaoTotal();
+    }
+
+    public function maiorPopulacao(){
+        $fMaiorPop = $this->listaPaises[0]->getPopulacao();
+        $sNomeMaiorPop = '';
+        foreach($this->listaPaises as $oObjPais){
+            if($oObjPais->getPopulacao() > $fMaiorPop){
+                $fMaiorPop = $oObjPais->getPopulacao();
+                $sNomeMaiorPop = $oObjPais->getNome();
+            }
+        }
+        return /*'Maior População - '.$sNomeMaiorPop.'-'.*/$fMaiorPop;
+    }
+
+    public function menorPopulacao(){
+        $fMenorPop = $this->listaPaises[0]->getPopulacao();
+        $sNomeMenorPop = '';
+        foreach($this->listaPaises as $oObjPais){
+            if($oObjPais->getPopulacao() < $fMenorPop){
+                $fMenorPop = $oObjPais->getPopulacao();
+                $sNomeMenorPop = $oObjPais->getNome();
+            }
+        }
+        return /*'Menor População - '.$sNomeMenorPop.'-'.*/ $fMenorPop;
+    }
+
+    public function maiorTerritorio(){
+        $fMaiorTerr     = $this->listaPaises[0]->getDimensao();
+        $sNomeMaiorTerr = '';
+        foreach($this->listaPaises as $oObjPais){
+            if($oObjPais->getDimensao() > $fMaiorTerr){
+                $fMaiorTerr     = $oObjPais->getDimensao();
+                $sNomeMaiorTerr = $oObjPais->getNome();
+            }
+        }
+        return $fMaiorTerr;
+    }
+
+    public function menorTerritorio(){
+        $fMenorTerr     = $this->listaPaises[0]->getDimensao();
+        $sNomeMenorTerr = '';
+        foreach($this->listaPaises as $oObjPais){
+            if($oObjPais->getDimensao() < $fMenorTerr){
+                $fMenorTerr     = $oObjPais->getDimensao();
+                $sNomeMenorTerr = $oObjPais->getNome();
+            }
+        }
+        return $fMenorTerr;
+    }
+
+    public function DiffTamTerr(){
+        return $this->maiorTerritorio() - $this->menorTerritorio();
+    }
 }
 ?>
---
+++ aulas/php/exercicios/exPHPpoo/exContinentes/exClassContinente.php
@@ -0,0 +1,41 @@
+<?php
+include_once 'classPais.php';
+include_once 'classContinente.php';
+
+$oBrasil = new Pais('BRA','Brasil',(float)145682355);
+$oBrasil->setPopulacao((float)24005522);
+
+$oChile = new Pais('CHI','Chile',(float)82355);
+$oChile->setPopulacao((float)335451);
+
+$oJapao = new Pais('JAP','Japão',(float)145687);
+$oJapao->setPopulacao((float)114556);
+
+$oRussia = new Pais('RUS','Russia',(float)41575235);
+$oRussia->setPopulacao((float)178982);
+
+$oAmericaSul = new Continente ("América do Sul");
+echo($oAmericaSul->adicionaPais($oChile));echo '<br>';
+echo($oAmericaSul->adicionaPais($oBrasil));echo '<br>';
+echo$oAmericaSul->dimensaoTotal();echo '<br>';
+echo$oAmericaSul->populacaoTotal();echo '<br>';
+echo$oAmericaSul->densidadeTotal();echo '<br>';
+echo$oAmericaSul->maiorPopulacao();echo '<br>';
+echo$oAmericaSul->menorPopulacao();echo '<br>';
+echo$oAmericaSul->maiorTerritorio();echo '<br>';
+echo$oAmericaSul->menorTerritorio();echo '<br>';
+echo$oAmericaSul->DiffTamTerr();echo '<br>';
+
+
+$oAsia = new Continente ('Ásia');echo '<br>';
+echo($oAsia->adicionaPais($oJapao));echo '<br>';
+echo($oAsia->adicionaPais($oRussia));echo '<br>';
+echo$oAsia->dimensaoTotal();echo '<br>';
+echo$oAsia->populacaoTotal();echo '<br>';
+echo$oAsia->densidadeTotal();echo '<br>';
+echo$oAsia->maiorPopulacao();echo '<br>';
+echo$oAsia->menorPopulacao();echo '<br>';
+echo$oAsia->maiorTerritorio();echo '<br>';
+echo$oAsia->menorTerritorio();echo '<br>';
+echo$oAsia->DiffTamTerr();echo '<br>';
+?>
